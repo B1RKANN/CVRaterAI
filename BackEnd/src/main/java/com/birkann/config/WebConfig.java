1c9e60161dd60162fbcd84cdd0fc80d4c419d28e
Birkan Boz <164794614+B1RKANN@users.noreply.github.com>
2025-03-23 16:41:11 +0300

    Cors Ayarı Yapıldı

--- BackEnd/src/main/java/com/birkann/config/WebConfig.java
+++ BackEnd/src/main/java/com/birkann/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedOrigins("http://127.0.0.1:5500", "https://cvraterai.com", "http://cvraterai.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
